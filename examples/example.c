99ac1b10c2f02db731b44746422678e521372443
Wirtos <Wirtos.new@gmail.com>
2020-04-28 13:42:35 +0300

    README and simpler example

--- examples/example.c
+++ examples/example.c
@@ -6,12 +6,20 @@
 struct f1_stack {
     int i;
     int res;
-    struct astate **states;
 };
 
-async f3(struct astate *state, int *args, void *locals) {
+async f4(struct astate *state, void *args, void *locals) {
+    async_begin(state);
+            puts("f4 call 1");
+            async_yield;
+            puts("f4 call 2");
+    async_end;
+}
+
+
+async f3(struct astate *state, int *args, int *locals) {
     int *i = locals;
-    int *res = args;
+    int *res = args; /* Pointer assignment from locals or args is fine outside async_begin, but value assignment isn't. */
     async_begin(state);
             for (*i = 0; *i < 3; (*i)++) {
                 printf("f3 %d\n", (*i) + 1);
@@ -27,40 +35,25 @@ async f2(struct astate *state, void *args, void *locals) {
     async_end;
 }
 
-async f4(struct astate *state, void *args, void *locals) {
-    async_begin(state);
-            puts("f4 call 1");
-            async_yield;
-            puts("f4 call 2");
-    async_end;
-}
-
 
 async f1(struct astate *state, char *args, struct f1_stack *locals) {
-    int *i = &locals->i;
-    int *res = &locals->res;
     async_begin(state);
-            for (*i = 0; *i < 3; (*i)++) {
-                printf("f0 %s %d\n", args, (*i) + 1);
-                fawait(async_new(f3, res, int));
-                printf("Result: %d\n", *res);
+            for (locals->i = 0; locals->i < 3; locals->i++) {
+                printf("f0 %s %d\n", args, (locals->i) + 1);
+                fawait(async_new(f3, &locals->res, int)); /* Create new coro from f3 and wait until it completes. */
+                printf("Result: %d\n", locals->res);
             }
-            locals->states = malloc(sizeof(astate *) * 2);
-            locals->states[0] = async_new(f4, NULL, ASYNC_NOLOCALS);
-            locals->states[1] = async_new(f4, NULL, ASYNC_NOLOCALS);
             fawait(async_vgather(2, async_new(f4, NULL, ASYNC_NOLOCALS), async_new(f4, NULL, ASYNC_NOLOCALS)));
-            fawait(async_gather(2, locals->states));
-            free(locals->states);
     async_end;
 }
 
 
 int main() {
-    async_loop_init();
+    async_loop_init(); /* Init event loop and create some tasks to run them later. */
     async_create_task(async_new(f1, "first", struct f1_stack));
     async_create_task(async_new(f2, NULL, ASYNC_NOLOCALS));
     async_create_task(async_new(f1, "second", struct f1_stack));
-    async_loop_run_forever();
+    async_loop_run_forever(); /* Block execution and run all tasks. */
     async_loop_destroy();
     return 0;
 }
